1f93012326194e27badb1e9082b3bec7d09efa4f
Kristina <kristina0x7@gmail.com>
2026-02-06 08:46:52 +0100

    Feature/tcp serversocket #4 (#10)

---
+++ src/main/java/org/example/SocketServer.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SocketServer {
+
+    static void createSocket() {
+        int port = 3000;
+
+        try (ServerSocket serverSocket = new ServerSocket(port, 64)) {
+
+            System.out.println("Server started at port: " + serverSocket.getLocalPort());
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                Thread.ofVirtual().start(() -> handleClient(socket));
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static void handleClient(Socket socket) {
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

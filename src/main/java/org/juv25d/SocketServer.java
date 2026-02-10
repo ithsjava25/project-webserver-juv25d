3035a4990a35934fcccbb4b4360e725f47e826da
WHITEROSE <firasmoussa60@gmail.com>
2026-02-10 11:42:30 +0100

    remove unused imports

--- src/main/java/org/juv25d/SocketServer.java
+++ src/main/java/org/juv25d/SocketServer.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class SocketServer {

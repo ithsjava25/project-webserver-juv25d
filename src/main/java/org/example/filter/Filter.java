43aa0a10851dd42f57a17b558efaf35314ed5079
Kristina M <kristina0x7@gmail.com>
2026-02-10 11:29:39 +0100

    add init and destroy to filter interface

--- src/main/java/org/example/filter/Filter.java
+++ src/main/java/org/example/filter/Filter.java
@@ -6,5 +6,7 @@ import org.example.http.HttpResponse;
 import java.io.IOException;
 
 public interface Filter {
+    default void init() {}
     void doFilter(HttpRequest req, HttpResponse res, FilterChain chain) throws IOException;
+    default void destroy() {}
 }

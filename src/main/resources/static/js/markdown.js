20213b180437d74d24eb655b4c76113daa24befb
Mats Rönnqvist <mats.f.ronnqvist@gmail.com>
2026-02-13 01:06:29 +0100

    Update index.html to include a nav bar at the top, styles.css for adjustments to frontend aspects, add readme.html to host readme.md, basic javascript for loading markdown language content.

---
+++ src/main/resources/static/js/markdown.js
@@ -0,0 +1,9 @@
+fetch("/readme.md")
+    .then(response => response.text())
+    .then(markdown => {
+        const container = document.getElementById("readme_content");
+        container.innerHTML = marked.parse(markdown);
+    })
+    .catch(error => {
+        console.error("Kunde inte ladda README.md", error);
+    });

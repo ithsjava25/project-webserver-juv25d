f2cef2af9736ebc6c25a755687aae288fcad0776
Annika Holmqvist <annika.holmqvist94@gmail.com>
2026-02-13 10:52:37 +0100

    WIP: Save current work

--- src/main/java/org/juv25d/App.java
+++ src/main/java/org/juv25d/App.java
@@ -4,6 +4,9 @@ import org.juv25d.filter.LoggingFilter;
 import org.juv25d.logging.ServerLogging;
 import org.juv25d.http.HttpParser;
 import org.juv25d.plugin.HelloPlugin;
+import org.juv25d.filter.RedirectFilter;
+import org.juv25d.filter.RedirectRule;
+import java.util.List;
 
 import java.util.logging.Logger;
 
@@ -13,6 +16,14 @@ public class App {
         HttpParser httpParser = new HttpParser();
 
         Pipeline pipeline = new Pipeline();
+        // Configure redirect rules
+        List<RedirectRule> redirectRules = List.of(
+            new RedirectRule("/old-page", "/new-page", 301),
+            new RedirectRule("/temp", "https://example.com/temporary", 302),
+            new RedirectRule("/docs/*", "/documentation/", 301)
+        );
+        pipeline.addFilter(new RedirectFilter(redirectRules));
+
         pipeline.addFilter(new LoggingFilter());
         pipeline.setPlugin(new HelloPlugin());
         pipeline.init();

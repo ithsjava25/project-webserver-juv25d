8f3a11c43d2e28f934930ce7a8057fdf83575cc4
Linda Eskilsson <eskilsson.linda@hotmail.se>
2026-02-16 13:23:44 +0100

    test(config-loader): add test skeleton for ConfigLoader

---
+++ src/test/java/org/juv25d/util/ConfigLoaderTest.java
@@ -0,0 +1,18 @@
+package org.juv25d.util;
+
+import org.junit.jupiter.api.Test;
+
+class ConfigLoaderTest {
+
+    @Test
+    void loadsValuesFromYaml() {
+    }
+
+    @Test
+    void usesDefaultsWhenServerKeysMissing() {
+    }
+
+    @Test
+    void throwsWhenYamlMissing() {
+    }
+}

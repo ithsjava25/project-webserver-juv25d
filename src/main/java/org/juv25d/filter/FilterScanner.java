aa7bf841146581f9b1a53b87ca9904f4c826bb83
Kristina M <kristina0x7@gmail.com>
2026-02-18 21:10:51 +0100

    feat(filters): introduce annotation-based filter registration

--- src/main/java/org/juv25d/filter/FilterScanner.java
+++ src/main/java/org/juv25d/filter/FilterScanner.java
@@ -1,5 +1,40 @@
 package org.juv25d.filter;
 
+import org.juv25d.Pipeline;
+import org.juv25d.filter.annotation.Global;
+import org.juv25d.filter.annotation.Route;
+
 public class FilterScanner {
 
+    public static void register(Object instance, Pipeline pipeline) {
+        Class<?> filterClass = instance.getClass();
+
+        if (!Filter.class.isAssignableFrom(filterClass)) {
+            return;
+        }
+
+        Global global = filterClass.getAnnotation(Global.class);
+        Route route = filterClass.getAnnotation(Route.class);
+
+        if (global != null && route != null) {
+            throw new IllegalStateException(
+                filterClass.getName() +
+                    " cannot be annotated with both @Global and @Route"
+            );
+        }
+
+        if (global != null) {
+            pipeline.addGlobalFilter((Filter) instance, global.order());
+        }
+
+        if (route != null) {
+            for (String pattern : route.value()) {
+                pipeline.addRouteFilter(
+                    (Filter) instance,
+                    route.order(),
+                    pattern
+                );
+            }
+        }
+    }
 }

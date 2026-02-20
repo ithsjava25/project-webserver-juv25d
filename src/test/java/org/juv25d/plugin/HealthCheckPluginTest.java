f8c801fa01c1ec439b99dd10fa4e92c44d519838
Mats Rönnqvist <mats.f.ronnqvist@gmail.com>
2026-02-20 21:59:07 +0100

    Add HealthCheckPlugin for server status endpoint

--- src/test/java/org/juv25d/plugin/HealthCheckPluginTest.java
+++ src/test/java/org/juv25d/plugin/HealthCheckPluginTest.java
@@ -23,12 +23,12 @@ class HealthCheckPluginTest {
         assertEquals(200, res.statusCode());
         assertEquals("OK", res.statusText());
         assertEquals("application/json", res.headers().get("Content-Type"));
-        
+
         String body = new String(res.body(), StandardCharsets.UTF_8);
         assertTrue(body.contains("\"status\": \"UP\""), "Body should contain status: UP");
         assertTrue(body.contains("\"timestamp\""), "Body should contain a timestamp");
         assertTrue(body.contains("\"server\": \"juv25d-webserver/1.0\""), "Body should contain server info");
-        
+
         String contentLength = res.headers().get("Content-Length");
         assertNotNull(contentLength, "Content-Length should be set");
         assertEquals(String.valueOf(res.body().length), contentLength);

59b13976a470c33365c0be682a736c291e4527f7
Fiona Friberg <fionafriberg@outlook.com>
2026-02-20 09:36:41 +0100

    Create ArchitectureTest

---
+++ src/test/java/org/juv25d/ArchitectureTest.java
@@ -0,0 +1,13 @@
+package org.juv25d;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import org.junit.jupiter.api.Test;
+
+@AnalyzeClasses(packages = "org.juv25d")
+public class ArchitectureTest {
+
+    @ArchTest
+    void ArchTest()
+    {}
+}

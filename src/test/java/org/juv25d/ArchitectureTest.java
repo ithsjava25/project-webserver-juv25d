442c6a5a97c76489b88332338f18bf7e333bdd02
Fiona Friberg <fionafriberg@outlook.com>
2026-02-20 15:02:58 +0100

    Create initial ArchRule

--- src/test/java/org/juv25d/ArchitectureTest.java
+++ src/test/java/org/juv25d/ArchitectureTest.java
@@ -2,12 +2,37 @@ package org.juv25d;
 
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+import com.tngtech.archunit.library.Architectures;
 import org.junit.jupiter.api.Test;
+
+import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
 
 @AnalyzeClasses(packages = "org.juv25d")
 public class ArchitectureTest {
 
+
     @ArchTest
-    void ArchTest()
-    {}
+    static final ArchRule rule =
+        ArchRuleDefinition.classes()
+            .that().haveSimpleName("ConnectionHandler")
+            .should().onlyBeAccessed().byClassesThat()
+            .haveSimpleName("Server")
+            .orShould().haveSimpleName("ConnectionHandler")
+            .orShould().haveSimpleName("DefaultConnectionHandlerFactory")
+            .orShould().haveSimpleName("ConnectionHandlerFactory");
 }
+
+//    @ArchTest
+//    public static final ArchRule lifecycleArchitecture = layeredArchitecture()
+//        .consideringAllDependencies()
+//
+//        .layer("Server").definedBy("org.juv25d.Server..")
+//        .layer("ConnectionHandler").definedBy("org.juv25d.ConnectionHandler..")
+//
+//        .whereLayer("ConnectionHandler").mayOnlyBeAccessedByLayers("Server");
+//
+//
+//
+//}

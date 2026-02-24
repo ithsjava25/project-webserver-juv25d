e0585d2ce31aa18c3bc4b7d6f91bb7befeab4a01
Simon Forsberg <simon.co.forsberg@gmail.com>
2026-02-24 16:06:43 +0100

    Add ForwardedHeaderFilterTest, verify request is passed when header is blank, verify IP is set when containing X-Forwarded-For header
    Co-authored-by: Mattias Hagström <mattiashagstrommusic@gmail.com>

---
+++ src/test/java/org/juv25d/filter/ForwardedHeaderFilterTest.java
@@ -0,0 +1,64 @@
+package org.juv25d.filter;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.juv25d.http.HttpRequest;
+import org.juv25d.http.HttpResponse;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.IOException;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class ForwardedHeaderFilterTest {
+
+    @Mock
+    private HttpRequest req;
+    @Mock
+    private HttpResponse res;
+    @Mock
+    private FilterChain chain;
+
+    private final String expectedForwardedHeader = "127.0.0.1, 83.2.0.12";
+    private final String expectedRemoteIp = "127.0.0.1";
+
+    @Test
+    void shouldSetRemoteIp_fromForwardedHeader() throws IOException {
+        // Arrange
+        ForwardedHeaderFilter filter = new ForwardedHeaderFilter();
+        Mockito.when(req.headers()).thenReturn(Map.of("X-Forwarded-For", expectedForwardedHeader));
+
+        ArgumentCaptor<HttpRequest> captor = ArgumentCaptor.forClass(HttpRequest.class);
+
+        // Act
+        filter.doFilter(req, res, chain);
+
+        // Assert
+        verify(chain).doFilter(captor.capture(), Mockito.eq(res));
+        assertEquals(expectedRemoteIp, captor.getValue().remoteIp());
+    }
+
+    @Test
+    void shouldPassOnRequest_ifHeaderNotPresent() throws IOException {
+        // Arrange
+        ForwardedHeaderFilter filter = new ForwardedHeaderFilter();
+        Mockito.when(req.remoteIp()).thenReturn("123.0.0.1");
+        Mockito.when(req.headers()).thenReturn(Map.of());
+
+        ArgumentCaptor<HttpRequest> captor = ArgumentCaptor.forClass(HttpRequest.class);
+
+        // Act
+        filter.doFilter(req, res, chain);
+
+        // Assert
+        verify(chain).doFilter(captor.capture(), Mockito.eq(res));
+        assertEquals("123.0.0.1", captor.getValue().remoteIp());
+    }
+
+}
